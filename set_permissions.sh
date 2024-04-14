8e203ccd7b0a1f32cf91b47a9b180426e4917a3f
Đức 3K <63569635+berusssv5@users.noreply.github.com>
2024-04-14 23:41:53 +0700

    Update set_permissions.sh

--- set_permissions.sh
+++ set_permissions.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/expect -f
-
-# Thiết lập các biến
 set code_dir "/code"
-set password "Mypassword@12345"
-
-# Thiết lập mật khẩu expect
+set password "huyduc123"
 spawn sudo chmod 777 $code_dir/*
-
-# Chờ cho sudo yêu cầu mật khẩu
 expect "Password:"
-
-# Gửi mật khẩu
 send "$password\r"
-
-# Chờ cho quá trình hoàn thành
 expect eof
